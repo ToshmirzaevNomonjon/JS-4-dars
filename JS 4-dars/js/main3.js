aa713c05a00771e03d74cbb7c8c2e0ce024ac64e
Islom Turgunov <147710629+ToshmirzaevNomonjon@users.noreply.github.com>
2023-10-16 18:47:15 +0500

    JS 4-dars
    27-guruh

---
+++ JS 4-dars/js/main3.js
@@ -0,0 +1,112 @@
+let wrap = document.getElementsByTagName("div").namedItem("wrap");
+let formGroup = document.getElementsByTagName("div").namedItem("form-group");
+let p1 = document.getElementsByTagName("p").namedItem("p1");
+let h1 = document.getElementsByTagName("h1").namedItem("h1");
+let hr = document.getElementsByTagName("hr").namedItem("hr");
+let input1 = document.getElementsByTagName("input").namedItem("input1");
+let input2 = document.getElementsByTagName("input").namedItem("input2");
+let input3 = document.getElementsByTagName("input").namedItem("input3");
+let input4 = document.getElementsByTagName("input").namedItem("input4");
+let p2 = document.getElementsByTagName("p").namedItem("p2");
+let day = document.getElementsByTagName("select").namedItem("day");
+let day1 = document.getElementsByTagName("select").namedItem("day1");
+let day2 = document.getElementsByTagName("select").namedItem("day2");
+let jinslar = document.getElementsByTagName("p").namedItem("jinslar");
+let jins = document.getElementsByTagName("input").namedItem("jins");
+let jins1 = document.getElementsByTagName("input").namedItem("jins1");
+let jins2 = document.getElementsByTagName("input").namedItem("jins2");
+let p3 = document.getElementsByTagName("p").namedItem("p3");
+let p4 = document.getElementsByTagName("p").namedItem("p4");
+let button = document.getElementsByTagName("button").namedItem("button")
+let p5 = document.getElementsByTagName("p").namedItem("p5");
+
+
+
+
+
+wrap.style.width = "1200px"
+wrap.style.margin = "0 auto"
+wrap.style.textAlign = "left"
+wrap.style.marginTop = "50px"
+
+formGroup.style.width = "500px";
+formGroup.style.margin = "0 auto";
+formGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+formGroup.style.padding = "1.2rem ";
+formGroup.style.boxSizing = "borderBox";
+
+h1.style.textAlign = "center"
+p1.style.textAlign = "center"
+
+hr.style.width = "544px"
+hr.style.marginLeft = "-24px"
+
+input1.style.width = "230px"
+input1.style.height = "30px"
+input1.style.marginTop = "10px"
+input1.style.borderRadius = "5px"
+
+input2.style.width = "230px"
+input2.style.height = "30px"
+input2.style.marginLeft = "20px"
+input2.style.borderRadius = "5px"
+
+input3.style.width = "492px"
+input3.style.height = "30px"
+input3.style.borderRadius = "5px"
+
+input4.style.width = "492px"
+input4.style.height = "30px"
+input4.style.borderRadius = "5px"
+
+p2.style.color = "grey"
+
+day.style.width = "150px"
+day.style.height = "30px"
+day.style.borderRadius = "5px"
+
+day1.style.width = "150px"
+day1.style.height = "30px"
+day1.style.borderRadius = "5px"
+day1.style.marginLeft = "20px"
+
+day2.style.width = "150px"
+day2.style.height = "30px"
+day2.style.borderRadius = "5px"
+day2.style.marginLeft = "20px"
+
+jinslar.style.color = "grey"
+
+jins.style.width = "150px"
+jins.style.height = "30px"
+jins.style.borderRadius = "5px"
+
+jins1.style.width = "150px"
+jins1.style.height = "30px"
+jins1.style.marginLeft = "8px"
+jins1.style.borderRadius = "5px"
+
+jins2.style.width = "150px"
+jins2.style.height = "30px"
+jins2.style.marginLeft = "8px"
+jins2.style.borderRadius = "5px"
+
+p3.style.fontSize = "13px"
+p3.style.color = "grey"
+
+p4.style.fontSize = "13px"
+p4.style.color = "grey"
+
+button.style.width = "200px"
+button.style.height = "40px"
+button.style.marginLeft = "150px"
+button.style.backgroundColor = "green"
+button.style.border = "1px solid green"
+button.style.color = "white"
+button.style.borderRadius = "5px"
+button.style.fontSize = "15px"
+button.style.fontWeight = "bold"
+
+p5.style.fontSize = "20px"
+p5.style.color = "blue"
+p5.style.textAlign = "center"
